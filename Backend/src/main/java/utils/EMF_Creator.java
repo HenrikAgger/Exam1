2ae135f05709f656b6196f2e5cb9cefdc1616571
HenrikAgger <cph-ha104@cphbusiness.dk>
2020-01-10 15:49:07 +0100

    first commit

--- Backend/src/main/java/utils/EMF_Creator.java
+++ Backend/src/main/java/utils/EMF_Creator.java
@@ -118,7 +118,7 @@ public class EMF_Creator {
         if (isDeployed) {
             user = System.getenv("USER");
             pw = System.getenv("PW");
-            connection_str = System.getenv("CONNECTION_STR_F2W1FRI");
+            connection_str = System.getenv("CONNECTION_STR_EXAM1");
         }
         /*
         On your server in /opt/tomcat/bin/setenv.sh   add the following WITH YOUR OWN VALUES
